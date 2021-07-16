276d7480f7f16943566db622ae9f63d612657cad
wilson <wilson.yu.cao@outlook.com>
2021-07-16 09:37:48 +0200

    fix formatting

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
 # Fibonacci number
 def F(n):
-  assert n>=0, 'assert n>=0'
+    assert n>=0, 'assert n>=0'
+    seq = []
 
-  if n == 0:
-    seq = [0]
-  elif n == 1:
-    seq = [0,1]
-  elif n > 1 :
-    seq = [0,1]
-    for i in range(2, n+1):
-      seq.append(seq[i-2] + seq[i-1])
+    if n == 0:
+        seq = [0]
+    elif n == 1:
+        seq = [0,1]
+    elif n > 1 :
+        seq = [0,1]
+        for i in range(2, n+1):
+            seq.append(seq[i-2] + seq[i-1])
 
-  return seq
+    return seq
