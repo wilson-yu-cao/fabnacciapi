2a5e80923244e7b341c9337eadf51a27dbe5ecc1
wilson <wilson.yu.cao@outlook.com>
2021-07-17 05:13:45 +0200

    Implement blacklist endpoint

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from flask.wrappers import Request
 from flask_restx import reqparse, inputs, Resource, Api
 import functions
 
+# This is only for dev or demo purpose. Such global cache
+# may cause unexpected behavior when Flask handles requests using multiple threads. 
+# For production, use a memory cache solution.
+fib_blacklist=set()
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -41,7 +46,7 @@ class fibonacci(Resource):
     @api.expect(parser)
     def get(self,n):
         """
-        Calculate Fibonacci sequence
+        Retrieve the Fibonacci sequence for the number, unless it's in the blacklist
         """
 
         args = self.parser.parse_args()
@@ -49,12 +54,39 @@ class fibonacci(Resource):
         size = args['size']
 
         start_time = time.perf_counter()
-        fab = functions.F_with_cache(n,page,size)
+        fib = functions.F_with_cache(n,page,size)
         end_time = time.perf_counter()
 
         result = {}
         result['duration']  = round((end_time - start_time) * 1000,2)
-        result['sequence']  = fab
+        result['sequence']  = fib
+
+        return result
+
+@api.route('/blacklist/<int:n>', endpoint='blacklist')
+class blacklist(Resource):
+    def post(self,n):
+        """
+        Create a blacklisted number
+        """
+        global fib_blacklist
+        fib_blacklist.add(n)
+
+        result = {}
+        result['blacklist_numbers']  = [str(element) for element in fib_blacklist]
+
+        return result
+
+    def delete(self,n):
+        """
+        Delete a blacklisted number
+        """
+        global fib_blacklist
+        if n in fib_blacklist:
+            fib_blacklist.remove(n)
+
+        result = {}
+        result['blacklist_numbers']  = [str(element) for element in fib_blacklist]
 
         return result
 
