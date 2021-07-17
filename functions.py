66965f9b5e523e97b280f46a22571f92df3898aa
wilson <wilson.yu.cao@outlook.com>
2021-07-17 04:17:01 +0200

    implement cache(only for dev purpose), and pagination

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 # Fibonacci number
+from os import truncate
+
+
 def F(n):
     assert n>=0, 'assert n>=0'
     seq = []
@@ -13,3 +16,23 @@ def F(n):
             seq.append(seq[i-2] + seq[i-1])
 
     return seq
+
+# Fibonacci number with global cache
+# This is only for dev or demo purpose. Such global cache
+# may cause unexpected behavior when Flask handles requests using multiple threads. 
+# For production, use a memory cache solution.
+fab_cache=[]
+def F_with_cache(n, page, size):
+    assert n>=0, 'assert n>=0'
+    assert page>=1, 'assert page>=1'
+    assert size>=1, 'assert size>=1'
+
+    global fab_cache
+
+    if len(fab_cache) <= n:
+        fab_cache = [str(element) for element in F(n)]
+ 
+    fab_split=fab_cache[:n+1]
+    ret=fab_split[(page-1)*size:page*size]
+ 
+    return ret
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import json
 from flask import Flask, jsonify
+from flask.wrappers import Request
 from flask_restx import reqparse, inputs, Resource, Api
 import functions
 
@@ -19,31 +20,41 @@ class ping(Resource):
         """
         return {'status': 'ok'}
 
-@api.route('/fibonacci')
+@api.route('/fibonacci/<int:n>', endpoint='fibonacci')
 class fibonacci(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        'n', 
-        type=inputs.int_range(low=0, high=4999), 
-        help='N [0..4999] in Fibonacci number.', 
-        required=True,
+        'page', 
+        type=inputs.int_range(low=1,high=10000000), 
+        default=1,
+        help='Page index',
+        required=False,
+        location='args')
+    parser.add_argument(
+        'size', 
+        type=inputs.int_range(low=1,high=10000000), 
+        default=100,
+        help='Page size',
+        required=False,
         location='args')
 
     @api.expect(parser)
-    def get(self):
+    def get(self,n):
         """
         Calculate Fibonacci sequence
         """
+
         args = self.parser.parse_args()
-        n = args['n']
+        page = args['page']
+        size = args['size']
 
         start_time = time.perf_counter()
-        fab = functions.F(n)
+        fab = functions.F_with_cache(n,page,size)
         end_time = time.perf_counter()
 
         result = {}
         result['duration']  = round((end_time - start_time) * 1000,2)
-        result['sequence']  = json.dumps(fab)
+        result['sequence']  = fab
 
         return result
 
